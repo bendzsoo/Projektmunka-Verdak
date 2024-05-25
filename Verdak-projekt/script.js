9f6d9b575b569ed75931f16f54e87c9628515c52
bendzsoo <csefalvay.benedek@students.jedlik.eu>
2024-05-25 14:25:53 +0200

    joooo a quiz

--- Verdak-projekt/script.js
+++ Verdak-projekt/script.js
@@ -1,122 +1,41 @@
-const questionContainer = document.getElementById("question-container");
-const questionElement = document.getElementById("question");
-const answerButtons = document.getElementById("answer-buttons");
-const nextButton = document.getElementById("next-btn");
-const restartButton = document.getElementById("restart-btn");
-const resultDiv = document.getElementById("result");
+function submitAnswers() {
+	var total = 10;
+	var score = 0;
+	
+	//Get User Input
+	var q1 = document.forms["quizForm"]["q1"].value;
+	var q2 = document.forms["quizForm"]["q2"].value;
+	var q3 = document.forms["quizForm"]["q3"].value;
+	var q4 = document.forms["quizForm"]["q4"].value;
+	var q5 = document.forms["quizForm"]["q5"].value;
+  var q6 = document.forms["quizForm"]["q6"].value;
+  var q7 = document.forms["quizForm"]["q7"].value;
+  var q8 = document.forms["quizForm"]["q8"].value;
+  var q9 = document.forms["quizForm"]["q9"].value;
+  var q10 = document.forms["quizForm"]["q10"].value;
+	
+	//Validation	
+	for(i=1; i<=total; i++) {
+		if(eval('q'+i) === null || eval('q'+i) === '') {
+			alert('You missed question '+ i);
+			return false;
+		}
+	}
+	
+	//Set Correct Answers
+	var answers = ["b","a","d","b","d","a","a","a","a","a"];
+	
+	//Check Answers
+	for(i=1; i<=total; i++) {
+		if(eval('q'+i) === answers[i-1]) {
+			score++;
+		}
+	}	
+	
+	//Display Results
+	var results = document.getElementById('results');
+	results.innerHTML = '<h3>You scored <span>'+score+'</span> out of <span>'+total+'</span></h3>';
+	alert('You scored '+score+' out of '+total);
 
-let shuffledQuestions, currentQuestionIndex, score;
-
-const questions = [
-  {
-    question: "What is 2 + 2?",
-    answers: [
-      { text: "4", correct: true },
-      { text: "22", correct: false },
-      { text: "222", correct: false },
-      { text: "2222", correct: false },
-    ],
-  },
-  {
-    question: "What does HTML stand for?",
-    answers: [
-      { text: "HyperText Markup Language", correct: true },
-      { text: "HighText Machine Language", correct: false },
-      { text: "HyperText Machine Language", correct: false },
-      { text: "HighText Markup Language", correct: false },
-    ],
-  },
-  {
-    question: "Which property is used to change the background color?",
-    answers: [
-      { text: "color", correct: false },
-      { text: "bgColor", correct: false },
-      { text: "background-color", correct: true },
-      { text: "background", correct: false },
-    ],
-  },
-  {
-    question: "Inside which HTML element do we put the JavaScript?",
-    answers: [
-      { text: "<js>", correct: false },
-      { text: "<javascript>", correct: false },
-      { text: "<script>", correct: true },
-      { text: "<scripting>", correct: false },
-    ],
-  },
-];
-
-startQuiz();
-
-function startQuiz() {
-  score = 0;
-  questionContainer.style.display = "flex";
-  shuffledQuestions = questions.sort(() => Math.random() - 0.5);
-  currentQuestionIndex = 0;
-  nextButton.classList.remove("hide");
-  restartButton.classList.add("hide");
-  resultDiv.classList.add("hide");
-  setNextQuestion();
-}
-
-function setNextQuestion() {
-  resetState();
-  showQuestion(shuffledQuestions[currentQuestionIndex]);
-}
-
-function showQuestion(question) {
-  questionElement.innerText = question.question;
-  question.answers.forEach((answer, index) => {
-    const inputGroup = document.createElement("div");
-    inputGroup.classList.add("input-group");
-
-    const radio = document.createElement("input");
-    radio.type = "radio";
-    radio.id = "answer" + index;
-    radio.name = "answer";
-    radio.value = index;
-
-    const label = document.createElement("label");
-    label.htmlFor = "answer" + index;
-    label.innerText = answer.text;
-
-    inputGroup.appendChild(radio);
-    inputGroup.appendChild(label);
-    answerButtons.appendChild(inputGroup);
-  });
-}
-
-function resetState() {
-  while (answerButtons.firstChild) {
-    answerButtons.removeChild(answerButtons.firstChild);
-  }
-}
-
-nextButton.addEventListener("click", () => {
-  const answerIndex = Array.from(
-    answerButtons.querySelectorAll("input")
-  ).findIndex((radio) => radio.checked);
-  if (answerIndex !== -1) {
-    if (shuffledQuestions[currentQuestionIndex].answers[answerIndex].correct) {
-      score++;
-    }
-    currentQuestionIndex++;
-    if (shuffledQuestions.length > currentQuestionIndex) {
-      setNextQuestion();
-    } else {
-      endQuiz();
-    }
-  } else {
-    alert("Please select an answer.");
-  }
-});
-
-restartButton.addEventListener("click", startQuiz);
-
-function endQuiz() {
-  questionContainer.style.display = "none";
-  nextButton.classList.add("hide");
-  restartButton.classList.remove("hide");
-  resultDiv.classList.remove("hide");
-  resultDiv.innerText = `Your final score: ${score} / ${shuffledQuestions.length}`;
+	return false;
 }
